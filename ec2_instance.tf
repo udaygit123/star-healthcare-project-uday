14c83e957f490aa2830d5dc9752420ff47972f01
AnnapoornaKaradi <122218758+AnnapoornaKaradi@users.noreply.github.com>
2023-03-28 19:40:01 +0530

    updated

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -10,8 +10,8 @@ resource "aws_instance" "demo-server" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update -y",
-      "sudo apt install docker.io -y",
+      "sudo apt-get update -y",
+      "sudo apt-get install docker.io -y",
       "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
       "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
       "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
