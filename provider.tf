38f5f580b933101fe66e660787daaab3f5c545f1
Annu <annapoornakaradi96@gmail.com>
2023-03-28 16:09:09 +0530

    added files

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1
+  access_key= "AKIA22GWLXBJKMHGMJOG"
+  secret_key= "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"
+}
