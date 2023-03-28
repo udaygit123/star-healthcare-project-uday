05394a3006170e7ce6671df3950c9cb26c4ba839
AnnapoornaKaradi <122218758+AnnapoornaKaradi@users.noreply.github.com>
2023-03-28 18:50:03 +0530

    Updated

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -12,13 +12,16 @@ resource "aws_instance" "demo-server" {
     inline = [
       "sudo apt update -y",
       "sudo apt install docker.io -y",
+      "sudo systemctl enable docker",
       "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
       "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
       "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
       "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
       "sudo chmod +x /home/ubuntu/kubectl",
       "sudo cp kubectl /usr/local/bin/kubectl",
-      "sudo usermod -aG docker ubuntu"
+      "sudo newgrp docker",
+      "sudo usermod -aG docker ubuntu",
+      "sudo systemctl restart docker"
     ]
     connection {
       type        = "ssh"
