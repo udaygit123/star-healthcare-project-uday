31340a4a1d582dcb437a3e9c5432b65265ae114c
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-20 04:43:46 +0530

    Update provider.tf

--- terraform_setup/provider.tf
+++ terraform_setup/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
  environment {
-        AWS_ACCESS_KEY_ID     = credentials('AKIAZEM74KA4DWVKAUEI')
-        AWS_SECRET_ACCESS_KEY = credentials('CV/Kt5SdfgHm3/raDRWbAD6peTRzEUVfQiq+9wL0Y')
+        AWS_ACCESS_KEY_ID     = credentials("AKIAZEM74KA4DWVKAUEI")
+        AWS_SECRET_ACCESS_KEY = credentials("CV/Kt5SdfgHm3/raDRWbAD6peTRzEUVfQiq+9wL0Y")
  }
 }
