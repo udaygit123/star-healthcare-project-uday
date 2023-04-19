c2cca8a3f483ffb6746db2ef42a49597e9d7511a
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-20 05:01:51 +0530

    Update provider.tf

--- terraform_setup/provider.tf
+++ terraform_setup/provider.tf
@@ -1,6 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAZEM74KA4DWVKAUEI"
-  secret_key = "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"
- }
+  access_key= "AKIAZEM74KA4DWVKAUEI"
+  secret_key= "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"
+}
+
 
