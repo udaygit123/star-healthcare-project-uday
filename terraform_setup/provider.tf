52a2a8c5c28544c31c08f7f61c5b622f7575d63b
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-20 04:54:20 +0530

    updt

--- terraform_setup/provider.tf
+++ terraform_setup/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAZEM74KA4DWVKAUEI
-  secret_key = "CV/Kt5SdfgHm3/raDRWbAD6peTRzEUVfQiq+9wL0Y"
+  access_key = "AKIAZEM74KA4DWVKAUEI"
+  secret_key = "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"
  }
 
