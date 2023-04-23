5fd0cf083ed8b7ec341212d19c264ce9847665b6
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-23 23:51:05 +0530

    Update main.tf

--- terraform_setup/main.tf
+++ terraform_setup/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-Server2" {
+resource "aws_instance" "test-server2" {
   ami                    = "ami-007855ac798b5175e"
   instance_type          = "t2.micro"
   key_name = "JenkinSerP1key"
