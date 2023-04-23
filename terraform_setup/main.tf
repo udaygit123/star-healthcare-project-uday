cd902a7c02ee61b37c55efa79ab085aaa55b3b21
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-23 22:23:56 +0530

    Update main.tf

--- terraform_setup/main.tf
+++ terraform_setup/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "Test-Server2" {
+resource "aws_instance" "test-Server2" {
   ami                    = "ami-007855ac798b5175e"
   instance_type          = "t2.medium"
   availability_zone      = "us-east-1"
@@ -12,24 +12,18 @@ resource "aws_instance" "Test-Server2" {
       host        = self.public_ip
       user        = "ubuntu"
       private_key = file("./JenkinSerP1key.pem")
+     
     }
-    provisioner "local-exec" {
-     command = "ansiblePlaybook installation: 'ansible', playbook: '/var/lib/jenkins/workspace/Health-care-Project/terraform_setup/ansible-playbook.yml"
-    
   provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install docker.io -y",
-      "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
-      "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
-      "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
-      "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
-      "sudo chmod +x /home/ubuntu/kubectl",
-      "sudo cp kubectl /usr/local/bin/kubectl",
-      "sudo usermod -aG docker ubuntu",
-     ]
-   
+    inline = [ "echo 'wait to start instance' "]
   }
+  tags = {
+    Name = "test-server1"
   }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server2.public_ip} > inventory "
+  }
+ provisioner "local-exec" {
+ command = "ansiblePlaybook installation: 'ansible', playbook: '/var/lib/jenkins/workspace/Health-care-Project/terraform_setup/ansible-playbook.yml"
+   } 
 }
-
