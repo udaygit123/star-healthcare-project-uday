8e8d6892d64e8d5a9ecdd1282dbd6b9610c41bb0
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-24 16:58:52 +0530

    Update main.tf

--- terraform_setup/main.tf
+++ terraform_setup/main.tf
@@ -23,6 +23,6 @@ resource "aws_instance" "test-server2" {
    command = " echo ${aws_instance.test-server2.public_ip} > inventory "
   }
  provisioner "local-exec" {
- command = "ansiblePlaybook disableHostKeyChecking: true, installation: 'ansible', inventory: '/etc/ansible/hosts', playbook: 'ansible-playbook.yml'"
+ command = "ansible-playbook /var/lib/jenkins/workspace/Health-care-Project/terraform_setup/ansible-playbook.yml"
    } 
 }
